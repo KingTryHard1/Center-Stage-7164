7869297b285c940f931870d80a50b6daa9f87d66
KingTryHard1 <codeshb7@gmail.com>
2023-10-26 15:48:45 -0400

    SafeHallow and MotorTest added. Updated DriveConstance

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class MotorTest extends OpMode {
+
+    DcMotorEx motor;
+
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, "crane");
+
+    }
+
+    @Override
+    public void loop() {
+        double power = gamepad1.right_stick_y;
+
+        motor.setPower(power);
+
+    }
+}
