a7eb798a1d1514af3f593b17f7128e85c58e3392
KingTryHard1 <codeshb7@gmail.com>
2024-02-01 15:45:10 -0500

    More auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/CraneHeightMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/CraneHeightMode.java
@@ -3,29 +3,26 @@ package org.firstinspires.ftc.teamcode.Testing;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.DriveConstance;
-
+import org.firstinspires.ftc.teamcode.Auto.AutonomousDriveConstance;
 @TeleOp
-public class CraneHeightMode extends DriveConstance {
+public class CraneHeightMode extends AutonomousDriveConstance {
 
     int lowestVal = 0;
     int highestVal = -2200;
 
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
         initRobot();
         linearLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+        while (opModeIsActive()) {
+            linearLiftToPos(1, -3000);
 
+            telemetry.addData("Highest Position: ", linearLift.getCurrentPosition());
+            telemetry.update();
+        }
 
-    }
-
-    @Override
-    public void loop() {
-        linearLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-        telemetry.addData("Highest Position: ", linearLift.getCurrentPosition());
-        telemetry.update();
     }
 }
