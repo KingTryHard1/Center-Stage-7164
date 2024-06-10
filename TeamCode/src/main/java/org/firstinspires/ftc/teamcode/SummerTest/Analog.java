24525246b7960b3843c4565f8f65877eefdaaeab
KingTryHard1 <codeshb7@gmail.com>
2024-06-10 19:21:11 -0400

    Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerTest/Analog.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SummerTest/Analog.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.CRServoImplEx;
 import com.qualcomm.robotcore.hardware.PwmControl;
 
 import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class Analog extends LinearOpMode {
@@ -20,25 +21,23 @@ public class Analog extends LinearOpMode {
         //AnalogInput AnalogServoTR = hardwareMap.get(AnalogInput.class, "Servo");
         //AnalogInput AnalogServoBL = hardwareMap.get(AnalogInput.class, "Servo");
         //AnalogInput AnalogServoBR = hardwareMap.get(AnalogInput.class, "Servo");
-        CRServoImplEx ServoTL = hardwareMap.get(CRServoImplEx.class, "ServoTL");
+        //CRServoImplEx ServoTL = hardwareMap.get(CRServoImplEx.class, "ServoTL");
         //CRServoImplEx ServoTR = hardwareMap.get(CRServoImplEx.class, "ServoTR");
         //CRServoImplEx ServoBL = hardwareMap.get(CRServoImplEx.class, "ServoBL");
         //CRServoImplEx ServoBR = hardwareMap.get(CRServoImplEx.class, "ServoBR");
 
         Dictionary<String, CRServoImplEx> SwerveServos = new Hashtable<>();
 
-        SwerveServos.put("ServoTL", ServoTL);
+        CRServoImplEx ServoTL;
+
+        SwerveServos.put("ServoTL", ServoTL = hardwareMap.get(CRServoImplEx.class, "Servo"));
         //SwerveServos.put("ServoTR", ServoTR);
         //SwerveServos.put("ServoBL", ServoBL);
         //SwerveServos.put("ServoBR", ServoBR);
 
-        for (int i = 0; SwerveServos.size()> i; i++){
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                SwerveServos.elements().asIterator().next().setPwmRange(new PwmControl.PwmRange(500, 2500));
-            }
-
-        }
+        Enumeration<String> SwerveServosAmount = SwerveServos.keys();
+        while (SwerveServosAmount.hasMoreElements())
+            SwerveServos.elements().nextElement().setPwmRange(new PwmControl.PwmRange(500, 2500));
 
         waitForStart();
         while (opModeIsActive()){
